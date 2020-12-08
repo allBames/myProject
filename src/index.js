15f69c16fd0907b488243d3a330f13b1d3e3abc3
balmasovas <balmasov@ek.apress.ru>
2020-12-08 21:16:12 +0500

    Создание класса store..

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, {addMessage, addNewMessage, updateNewMessage, addPost, subscribe} from './redux/State'
+import store from './redux/State'
 import {BrowserRouter} from "react-router-dom";
 
 export let rerender = (state) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} addPost={addPost} addNewMessage={addNewMessage} addMessage={addMessage} updateNewMessage={updateNewMessage}/>
+                <App state={store.getState()}
+                     addPost={store.addPost.bind(store)}
+                     addNewMessage={store.addNewMessage.bind(store)}
+                     addMessage={store.addMessage.bind(store)}
+                     updateNewMessage={store.updateNewMessage.bind(store)}/>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-rerender(state);
+rerender(store.getState());
 
-subscribe(rerender);
+store.subscribe(rerender);
 
 reportWebVitals();
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,89 +1,89 @@
-let rerender = () => {
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: 1, message: 'Первый пост', like: 100},
+                {id: 2, message: 'Второй пост', like: 200}
+            ],
+            newPostMessage: ['Привет, мир!!!']
+        },
+        dialogsPage: {
+            dialogsData: [
+                {id: 1, name: 'Иван'},
+                {id: 2, name: 'Антон'},
+                {id: 3, name: 'Анна'},
+                {id: 4, name: 'Ксения'},
+                {id: 5, name: 'Никита'}
+            ],
+            messagesData: [
+                {id: 1, message: 'Новое сообщение1'},
+                {id: 2, message: 'Новое сообщение2'},
+                {id: 3, message: 'Новое сообщение3'},
+                {id: 4, message: 'Новое сообщение4'}
+            ],
+            newTextMessage: ['']
+        },
+        navbar: {
+            friends:
+                [
+                    {
+                        id: 1,
+                        name: 'Аня',
+                        avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
+                    },
+                    {
+                        id: 2,
+                        name: 'Ваня',
+                        avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
+                    },
+                    {
+                        id: 3,
+                        name: 'Миша',
+                        avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
+                    }
+                ]
 
-}
+        }
+    },
+    getState() {
+        return this._state
+    },
+    rerender(){
+        console.log('123')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostMessage,
+            like: 0
+        };
+        this._state.profilePage.postsData.push(newPost)
+        this._state.profilePage.newPostMessage = ''
+        this.rerender(this._state);
 
-let state = {
-    profilePage: {
-        postsData: [
-            {id: 1, message: 'Первый пост', like: 100},
-            {id: 2, message: 'Второй пост', like: 200}
-        ],
-        newPostMessage: ['Привет, мир!!!']
     },
-    dialogsPage: {
-        dialogsData: [
-            {id: 1, name: 'Иван'},
-            {id: 2, name: 'Антон'},
-            {id: 3, name: 'Анна'},
-            {id: 4, name: 'Ксения'},
-            {id: 5, name: 'Никита'}
-        ],
-        messagesData: [
-            {id: 1, message: 'Новое сообщение1'},
-            {id: 2, message: 'Новое сообщение2'},
-            {id: 3, message: 'Новое сообщение3'},
-            {id: 4, message: 'Новое сообщение4'}
-        ],
-        newTextMessage: ['']
+    addNewMessage(newText) {
+        this._state.profilePage.newPostMessage = (newText)
+        this.rerender(this._state);
     },
-    navbar: {
-        friends:
-            [
-                {
-                    id: 1,
-                    name: 'Аня',
-                    avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
-                },
-                {
-                    id: 2,
-                    name: 'Ваня',
-                    avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
-                },
-                {
-                    id: 3,
-                    name: 'Миша',
-                    avatar: 'https://i.pinimg.com/originals/cf/67/b2/cf67b21b83d577a1b5a223a468f8754d.jpg'
-                }
-            ]
+    addMessage() {
+        let newMessage = {
+            id: 5,
+            message: this._state.dialogsPage.newTextMessage
+        };
+        this._state.dialogsPage.messagesData.push(newMessage)
+        this._state.dialogsPage.newTextMessage = ''
+        this.rerender(this._state)
+    },
+    updateNewMessage(newText) {
+        this._state.dialogsPage.newTextMessage = (newText)
+        this.rerender(this._state)
+    },
+    subscribe(observer) {
+        this.rerender = observer
 
     }
 }
-window.state = state;
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostMessage,
-        like: 0
-    };
-    state.profilePage.postsData.push(newPost)
-    state.profilePage.newPostMessage = ''
-    rerender(state);
-
-}
-
-export let addNewMessage = (newText) => {
-    state.profilePage.newPostMessage = (newText)
-    rerender(state);
-}
-
-export let addMessage = () => {
-    let newMessage = {
-        id: 5,
-        message: state.dialogsPage.newTextMessage
-    };
-    state.dialogsPage.messagesData.push(newMessage)
-    state.dialogsPage.newTextMessage = ''
-    rerender(state)
-}
-
-export let updateNewMessage = (newText) => {
-    state.dialogsPage.newTextMessage = (newText)
-    rerender(state)
-}
-
-export let subscribe = (observer) => {
-    rerender = observer
-}
-
-export default state;
+window.store = store;
+export default store;
