9a0842dcc67a9c44fb4d647effa5a4ba49e037f9
balmasovas <balmasov@ek.apress.ru>
2020-12-13 02:43:12 +0500

    ..

--- src/components/Dialogs/DialogListContainer.jsx
+++ src/components/Dialogs/DialogListContainer.jsx
@@ -3,26 +3,6 @@ import {addMessageActionCreator, changeMessageActionCreator} from "../../redux/D
 import DialogList from "./DialogList";
 import {connect} from "react-redux";
 
-
-// function DialogListContainer(props) {
-//
-//     let newMessage = () => {
-//         props.dispatch(addMessageActionCreator())
-//     }
-//
-//     let changeNewMessage = (text) => {
-//         props.dispatch(changeMessageActionCreator(text))
-//     }
-//
-//     return (
-//         <DialogList addMessageActionCreator={newMessage}
-//                     changeMessageActionCreator={changeNewMessage}
-//                     newTextMessage={props.state.newTextMessage}
-//                     dialogsData={props.state.dialogsData}
-//                     messagesData={props.state.messagesData}/>
-//     );
-// }
-
 let mapStateToProps = (state) => {
     return {
         newTextMessage: state.dialogsPage.newTextMessage,
--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,37 +1,15 @@
 import React, { Component } from 'react';
-import {addPostActionCreator, changePostActionCreator} from "../../../redux/ProfileReducer";
 import {connect} from "react-redux";
 import MyPosts from "./MyPosts";
-
-// function NewPostsContainer() {
-
-//     let addPost = () => {
-//         props.dispatch(addPostActionCreator())
-//     }
-//
-//     let changePost = (text) => {
-//         props.dispatch(changePostActionCreator(text))
-//     }
-//
-//     return <NewPosts addPostActionCreator={addPost} changePostActionCreator={changePost} newPostMessage={props.newPostMessage}/>
-//}
 
 let mapStateToProps = (state) => {
     return {
         postsData: state.profilePage.postsData,
-        profilePage: state.profilePage,
-        newPostMessage: state.profilePage.newPostMessage
     }
 }
 
 let mapDispatchToProps = (dispatch) => {
     return {
-        addPost: () => {
-            dispatch(addPostActionCreator())
-        },
-        changePost: (text) => {
-            dispatch(changePostActionCreator(text))
-        }
     }
 }
 
